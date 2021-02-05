9cc30f743e2f8e22b4ab5d87af32f655f2e635af
Yuriy Lipchey <xaosmmm@gmail.com>
2021-02-05 22:49:41 +0200

    project finished

--- script.js
+++ script.js
@@ -5,9 +5,22 @@ const input = document.querySelector('#item-input')
 form.addEventListener('submit', event =>
 {
     event.preventDefault()
+
+    //create a new item
     const item = document.createElement('div')
     item.innerText = input.value
     item.classList.add('list-item')
 
+    //add item to the list
     list.appendChild(item)
+
+    //setup event listener
+    item.addEventListener('click', () =>
+    {
+        list.removeChild(item) //remove item from the list
+        //item.remove() - alternative way to remove item
+    })
+
+
+    input.value = ''
 })
